529c9a1c69ae3aaeb6f85daafd6cd7e14dc29308
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-27 23:16:56 +0600

    refactor component AddMessageForm;

--- src/components/AddMessageForm/AddMessageForm.tsx
+++ src/components/AddMessageForm/AddMessageForm.tsx
@@ -1,45 +1,14 @@
-import React, {ChangeEvent, useState} from 'react';
-import {PostType} from '../../types';
+import React, { ChangeEvent } from 'react';
+import { PostType } from '../../types';
 
-interface Props {
-  onSubmit: (post: PostType) => void;
+interface AddMessageFormProps {
+  post: PostType;
+  onFieldChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  onFormSubmit: (event: React.FormEvent) => void;
 }
 
-const AddMessageForm: React.FC<Props> = ({onSubmit}) => {
-  const [postData, setPostData] = useState({
-    title: '',
-    message: '',
-  });
-
-  const onFieldChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const {name, value} = event.target;
-
-    setPostData((prevState) => {
-      return {
-        ...prevState,
-        [name]: value,
-      }
-    });
-  };
-
-  const onFormSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-
-    const post: PostType = {
-      ...postData,
-      id: Math.random().toString(),
-      datetime: Date.now().toLocaleString(),
-    }
-    onSubmit(post);
-    setPostData({
-      title: '',
-      message: '',
-    });
-  };
-
+const AddMessageForm: React.FC<AddMessageFormProps> = ({ post, onFieldChange, onFormSubmit}) => {
   return (
-    <div className="container mt-3">
-      <h4>Add new post</h4>
       <form onSubmit={onFormSubmit}>
         <div className="form-group">
           <label htmlFor="title">Title</label>
@@ -50,7 +19,7 @@ const AddMessageForm: React.FC<Props> = ({onSubmit}) => {
             required
             className="form-control"
             onChange={onFieldChange}
-            value={postData.title}
+            value={post.title}
           />
         </div>
         <div className="form-group mt-3">
@@ -61,14 +30,13 @@ const AddMessageForm: React.FC<Props> = ({onSubmit}) => {
             required
             className="form-control"
             onChange={onFieldChange}
-            value={postData.message}
+            value={post.message}
           />
         </div>
         <button type="submit" className="btn btn-primary mt-4">
           Save post
         </button>
       </form>
-    </div>
   );
 };
 
