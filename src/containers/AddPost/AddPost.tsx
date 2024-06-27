409d5360866f365df1fa98fe7e720d86dd0018ec
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-27 23:18:30 +0600

    refactor component AddPost; added states;

--- src/containers/AddPost/AddPost.tsx
+++ src/containers/AddPost/AddPost.tsx
@@ -1,16 +1,60 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
+import { PostType } from '../../types';
+import { useNavigate } from 'react-router-dom';
 import AddMessageForm from '../../components/AddMessageForm/AddMessageForm';
-import {PostType} from '../../types';
+import axiosApi from '../../axiosApi';
+import Spinner from '../../components/Spinner/Spinner';
 
-interface AddPostProps {
-  onCreate: (post: PostType) => void;
-}
+const AddPost = () => {
+  const [post, setPost] = useState<PostType>({
+    title: '',
+    message: '',
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-const AddPost: React.FC<AddPostProps> = ({onCreate}) => {
+  const onFieldChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+
+    setPost((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const onFormSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const newPost = {
+      post,
+    };
+
+    try {
+      await axiosApi.post('/posts.json', newPost);
+    } finally {
+      setIsLoading(false);
+      navigate('/');
+    }
+  };
+
+  let form = (
+    <AddMessageForm
+      post={post}
+      onFieldChange={onFieldChange}
+      onFormSubmit={onFormSubmit}
+    />
+  );
+
+  if (isLoading) {
+    form = <Spinner />;
+  }
+
   return (
-    <>
-      <AddMessageForm onSubmit={onCreate} />
-    </>
+    <div className="container mt-3">
+      <h4>Add new post</h4>
+      {form}
+    </div>
   );
 };
 
