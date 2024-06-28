9727eafc26229832875fff088b9b4b7dcca384fd
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-29 00:13:46 +0600

    added all Routes to App;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
+import Posts from './containers/Posts/Posts';
+import {Route, Routes} from 'react-router-dom';
+import AddPost from './containers/AddPost/AddPost';
+import PostPage from './containers/PostPage/PostPage';
+import EditPost from './containers/EditPost/EditPost';
+import About from './containers/About/About';
+import Contacts from './containers/Contacts/Contacts';
 
 const App = () => {
 
   return (
     <>
-        Hello, world!
+      <header>
+        <Toolbar/>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Posts />}/>
+          <Route path="/posts/:id" element={<PostPage />} />
+          <Route path="/posts/:id/edit" element={<EditPost />} />
+          <Route path="/posts/add" element={<AddPost />}/>
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </main>
     </>
   );
 };
