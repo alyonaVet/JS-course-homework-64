ec7c6ad6791e27b158555540c9d7346e985f2938
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-28 16:19:54 +0600

    created component FullPost;

---
+++ src/components/Post/FullPost.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {FullPostType} from '../../types';
+
+interface FullPostProps {
+  post: FullPostType;
+  onEdit: () => void;
+  onDelete: () => void;
+}
+
+const FullPost: React.FC<FullPostProps> = ({post, onEdit, onDelete}) => {
+  return (
+    <div className="d-flex flex-column align-items-start mb-3 border rounded-1 p-3">
+      <div className="text-secondary">Created on: {post.datetime}</div>
+      <h4 className="fw-medium my-3">{post.post.title}</h4>
+      <p className="mt-2 mb-5">{post.post.message}</p>
+      <div>
+        <button className="btn btn-secondary me-3 px-4" onClick={onEdit}>Edit</button>
+        <button className="btn btn-danger" onClick={onDelete}>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+export default FullPost;
