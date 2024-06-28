87dac2617d772b6946c6f13b5cc25e6a531417e6
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-28 23:48:23 +0600

    created container About;

---
+++ src/containers/About/About.tsx
@@ -0,0 +1,23 @@
+const About = () => {
+  return (
+    <div className="container mt-3">
+      <h2>About This App</h2>
+      <p>
+        Welcome to application "My Blog"! This app allows you to create, view, edit, and delete posts. It is designed to be simple and intuitive, making it easy for users to manage their content.
+      </p>
+      <h3>Features</h3>
+      <ul>
+        <li>Create new posts with a title and message</li>
+        <li>View a list of all posts</li>
+        <li>Click on a post to see the full content</li>
+        <li>Edit existing posts to update their content</li>
+        <li>Delete posts that are no longer needed</li>
+      </ul>
+      <p>
+        We hope you find this app useful and easy to use. If you have any questions or feedback, please feel free to contact us.
+      </p>
+    </div>
+  );
+};
+
+export default About;
