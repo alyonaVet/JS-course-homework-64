916c8aeeec5a0b156d50b93c57b4602aaa3bffad
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-28 23:48:36 +0600

    created container Contacts;

---
+++ src/containers/Contacts/Contacts.tsx
@@ -0,0 +1,18 @@
+const Contacts = () => {
+  return (
+    <div className="container mt-3">
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions, feedback, or need support, feel free to contact us using the information below:
+      </p>
+      <h3>Email</h3>
+      <p>
+        <a href="mailto:myblog@gmail.com">myblog@gmail.com</a>
+      </p>
+      <h3>Phone</h3>
+      <p>+996555505050</p>
+    </div>
+  );
+};
+
+export default Contacts;
