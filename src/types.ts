97813ca61f46b191eda39325f8a71fbd2b3a018b
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-28 16:17:52 +0600

    added PostData, FullPostType, ApiFullPostType types;

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,28 @@
-export interface PostType {
+export interface PostData {
   id: string;
   title: string;
   datetime: string;
   message: string;
+}
+export interface PostType {
+  title: string;
+  message: string;
+}
+
+export interface FullPostType {
+  datetime: string;
+  post: {
+    title: string;
+    message: string;
+  };
+}
+
+export interface ApiFullPostType {
+  [key: string]: {
+    datetime: string;
+    post: {
+      title: string;
+      message: string;
+    };
+  };
 }
